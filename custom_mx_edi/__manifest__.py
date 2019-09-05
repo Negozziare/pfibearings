00bfafbde4b61e0ba73d8957da677f5ffecbd162
Developer <dev@test.com>
2019-09-05 20:38:33 +0530

    [IMP]Improve the field visiblity based on company onchange.

--- custom_mx_edi/__manifest__.py
+++ custom_mx_edi/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'custom EDI for Mexico',
-    'version': '1.1',
+    'version': '12.0',
     'summary': 'Odoo Mexico Localization for Product and Partner',
     'sequence': 30,
     'description': """
@@ -11,8 +11,10 @@
     'category': 'COA of Mexico',
     'depends': ['l10n_mx_edi'],
     'data': [
+        'security/hide_field.xml',
         'views/edi_maxico_group.xml',
         'views/inherit_maxico_edi_view.xml',
+        'views/res_compnay.xml',
     ],
     'demo': [
     ],
---
+++ custom_mx_edi/models/res_company.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields,api
+  
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    group_hide_field = fields.Boolean('Show Field')
+
+from odoo import models,fields,api
+  
+class res_parner(models.Model):
+    _inherit = 'res.partner'
+
+    is_show = fields.Boolean('Show Field', compute='_compute_field')
+
+    def _compute_field(self):
+        for record in self:
+            record.is_show = self.env.user.company_id.group_hide_field
+        
